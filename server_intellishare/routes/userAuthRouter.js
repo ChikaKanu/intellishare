606aa9df478b4b95ab6daf76056802eecb364fcc
ChikaKanu <chika.kanu@procompletions.com>
2020-01-23 06:06:23 +0000

    user register validation ongoing

--- server_intellishare/routes/userAuthRouter.js
+++ server_intellishare/routes/userAuthRouter.js
@@ -1,20 +1,48 @@
 const userRouter = require('express').Router();
 const User = require('../model/User');
 
+//Validation
+const Joi = require("@hapi/joi");
+
+const schema = Joi.object({
+    name: Joi.string()
+        .min(4)
+        .required(),
+    email: Joi.string()
+        .min(6)
+        .required()
+        .email(),
+    password: Joi.string()
+        .min(8)
+        .required(),
+    address: Joi.string()
+        .min(8)
+        .required(),
+    postcode: Joi.string()
+        .min(7)
+        .max(7)
+        .required()
+});
+
 userRouter.post('/register', async (req, res) => {
+    //lets validate data before we create a user
+        const {error} = schema.validate(req.body);
+        if(error){
+            return res.status(400).send(error.details[0].message)
+        }
         const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        address: req.body.address,
-        postcode: req.body.postcode
-    });
-    try{
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            address: req.body.address,
+            postcode: req.body.postcode
+        });
+        try{
         const savedUser = await user.save();
-        res.send(savedUser);
-    }catch(err){
-        res.status(400).send(err)
-    }
+            res.send(savedUser);
+        }catch(err){
+            res.status(400).send(err)
+        }
 });
 
 userRouter.patch("/:userId", async (req, res) => {
